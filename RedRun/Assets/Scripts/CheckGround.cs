d3a777c5820ce39a5618b8c51faa9dec9bb70d40
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2019-07-31 07:20:38 +0200

    Actualizacion
    Se cambio nombre del Juego
    Se cambio el sprite del personaje por el helmet hero
    Se agrego un script y se creo dentro del player un gameobject para gestionar el movimiento del personaje y que no tuviera un bug

---
+++ RedRun/Assets/Scripts/CheckGround.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Player_Controller player;
+    void Start()
+    {
+        player =GetComponentInParent<Player_Controller>();
+    }
+
+   void OnCollisionStay2D(Collision2D col) {
+       if (col.gameObject.tag == "Ground")
+       {
+           player.grounded = true;
+       }
+       player.grounded = true;
+   }
+
+   void  OnCollisionExit2D(Collision2D col) {
+       if (col.gameObject.tag == "Ground")
+       {
+           player.grounded = false;
+       }
+       player.grounded = false;
+   }
+
+}
---
+++ RedRun/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+        // Start is called before the first frame update
+        public float Speed = 2f;
+        private Rigidbody2D rb2D;
+        public bool grounded;
+        public float MaxSpeed = 5f;
+        private Animator anim;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rb2D.velocity.x));
+        anim.SetBool("Grounded",grounded);
+    }
+    void FixedUpdate() {
+     float h = Input.GetAxis("Horizontal");
+
+    rb2D.AddForce(Vector2.right * Speed * h);
+
+        float limitedSpeed = Mathf.Clamp(rb2D.velocity.x, -MaxSpeed,MaxSpeed );
+        rb2D.velocity = new Vector2(limitedSpeed, rb2D.velocity.y);
+        Debug.Log(rb2D.velocity.x);
+
+            //De esta manera puedo cambiar la localizacion el la que el player se encuentra caminando   
+        if(h>0.1f){
+            transform.localScale = new Vector3(1f,1f,1f);
+        }
+        if (h<-0.1f)
+        {
+            transform.localScale = new Vector3(-1f,1f,1f);
+        }
+    }
+    
+}
